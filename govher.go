49cb717de9865ae639b453f4c751f0112f243233
Chris Dawson <xrdawson@gmail.com>
2014-06-25 13:23:06 -0700

    Rework github

--- govher.go
+++ govher.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
-
+	"strings"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	path := ""
 	path = r.URL.Path[:]
 	fmt.Println( "Path: " + path )
+	// Replace github with github.com
+	path = strings.Replace( path, "github", "github.com" )
 	http.Redirect(w, r, "http://" + path, http.StatusFound)
         return
 	// fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
